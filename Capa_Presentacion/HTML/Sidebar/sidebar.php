c8e11db7d80459b05ab20b62337e95bfa9e9de64
AlejandroRBC <alejandrobernas5@gmail.com>
2025-09-23 00:05:33 -0400

    version2.0

--- Capa_Presentacion/HTML/Sidebar/sidebar.php
+++ Capa_Presentacion/HTML/Sidebar/sidebar.php
@@ -1,15 +1,11 @@
-
-
 <div class="sidebar">
     <div class="sidebar-cabeza">
         <h2 id="toggleSidebar"><span>CineMax</span></h2>
     </div>
     <ul class="sidebar-menu">
         <li><a href="<?= BASE_URL ?>index.php" class="seleccion_activa"><i>🏠</i><span>Inicio</span></a></li>
-        <li><a href="#"><i>🏢</i><span>Escoge tu sucursal</span></a></li>
         <li><a href="#"><i>👤</i><span>Registrarse</span></a></li>
         <li><a href="#"><i>🚪</i><span>Iniciar Sesión</span></a></li>
-        <li><a href="#"><i>⭐</i><span>Ver mis puntos</span></a></li>
         <li><a href="<?= BASE_URL ?>Capa_Presentacion/PHP/venta-transaccion/comprar.php"><i>🎟️</i><span>Comprar Boletos</span></a></li>
         <li><a href="#"><i>🎁</i><span>Promociones del día</span></a></li>
         <li><a href="<?= BASE_URL ?>Capa_Presentacion/PHP/Administracion/Sesion.php"><i>🔐</i><span>Administrador</span></a></li>
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <link rel="stylesheet" href="Capa_presentacion/CSS/Sidebar/sidebar.css">
+<link rel="stylesheet" href="Capa_presentacion/CSS/Inicio/card.css">
 <script src="Capa_presentacion/JAVASCRIPT/Sidebar/sidebar.js"></script>
 <?php 
     define("BASE_URL", "/CineBackend/");
     include('Capa_Presentacion/HTML/Sidebar/sidebar.php');
+    include_once __DIR__ . "/Capa_Datos/conexionBD/conexion.php";
+    include_once __DIR__ . "/Capa_Datos/SQL/CRUDPeliculas.php";
+    $peliculas = ListarPeliculasHabilitadas($conexion);
 ?>
 <div class="contenido-principal">
     <div class="content-cabeza">
@@ -11,5 +15,35 @@
     </div>
     <div class="contenido-cuerpo">
         <h2>Cartelera De peliculas</h2>
+        <div class="cartelera">
+        <?php while($p = $peliculas->fetch_assoc()): ?>
+            <div class="card"> 
+                <div class="header"> 
+                    <div class="content">
+                        <span class="titulo"><?= $p['nom_pel'] ?></span> 
+                        <p class="descripcion">
+                            <strong>Lenguaje</strong> <?= $p['lenguaje'] ?>
+                        </p> 
+                        <p class="descripcion">
+                            <strong>Formato</strong> <?= $p['formato'] ?>
+                        </p> 
+                        <p class="descripcion">
+                            <strong>Categoría</strong> <?= $p['categoria'] ?>
+                        </p> 
+    
+                    </div> 
+                    <div class="acciones">
+                            <button type="button" onclick="Hola()" class="Enter">Informacion</button> 
+                            <button type="button" onclick="Hola()" class="Enter2">Reseñas</button> 
+                    </div> 
+                </div> 
+            </div>
+        <?php endwhile; ?>    
+        </div>
     </div>
 </div>
+<script>
+    function Hola(){
+        console.log("Hola mundo");
+    }
+</script>
